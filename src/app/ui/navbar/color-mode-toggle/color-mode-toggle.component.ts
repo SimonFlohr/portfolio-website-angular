b8c2d9632d10a8c8909bb51f75ce45269b4bd2d3
Simon Flohr <sflohr2@wgu.edu>
2025-05-10 23:48:19 -0500

    feat: add navbar UI components
    - Scaffolds the initial navbar structure and its sub-components:
        + Logo
        + Links
        + Color Mode Toggle
        + Mobile Toggle
    
    These components provide the basic layout and functionality
    for the application's navigation bar.

---
+++ src/app/ui/navbar/color-mode-toggle/color-mode-toggle.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-color-mode-toggle',
+  imports: [],
+  templateUrl: './color-mode-toggle.component.html',
+  styleUrl: './color-mode-toggle.component.css'
+})
+export class ColorModeToggleComponent {
+
+}
---
+++ src/app/ui/navbar/mobile-toggle/mobile-toggle.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mobile-toggle',
+  imports: [],
+  templateUrl: './mobile-toggle.component.html',
+  styleUrl: './mobile-toggle.component.css'
+})
+export class MobileToggleComponent {
+
+}
