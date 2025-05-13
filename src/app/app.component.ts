ab6c7eb96190d48a1959d84cd554bdefa433c33c
Simon Flohr <sflohr2@wgu.edu>
2025-05-12 23:52:14 -0500

    feat: set initial dark mode and background color
    - Configures the application to initialize in dark mode and sets the background color accordingly based on the value stored in local storage.
    
    This enhances the user experience by providing a consistent visual theme upon initial load.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './ui/navbar/navbar.component';
 
@@ -11,6 +11,31 @@ import { NavbarComponent } from './ui/navbar/navbar.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'portfolio-website-angular';
+export class AppComponent implements OnInit {
+
+  isDarkMode: boolean | undefined = undefined;
+
+  ngOnInit(): void {
+    this.toggleDarkMode(); // Set initial display mode to dark mode, and save it to local storage
+    // Set the background color based on the initial display mode
+    switch(localStorage.getItem('isDarkMode')) {
+      case 'true':
+        document.body.style.backgroundColor = 'var(--neutral-black)';
+        break;
+      case 'false':
+        document.body.style.backgroundColor = 'var(--neutral-white)';
+        break;
+      default:
+        document.body.style.backgroundColor = 'var(--neutral-black)';
+        break;
+    }
+  }
+
+  //  Toggle between dark mode and light mode
+  // TODO -> Implement dark mode functionality
+  toggleDarkMode(): void {
+    this.isDarkMode = true;
+    localStorage.setItem('isDarkMode', 'true');
+  }
+
 }
