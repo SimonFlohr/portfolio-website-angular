38fd588a1f2c6e620ce031c9ca54f430d2e18d55
Simon Flohr <sflohr2@wgu.edu>
2025-05-13 02:22:18 -0500

    feat: add navigation links to the navbar
    - Replaces placeholder text with functional navigation links in the navbar.
        + These links include "About", "Education", "Projects", and "Contact".
    
    Improves the user experience by providing clear navigation options.

--- src/app/ui/navbar/links/links.component.ts
+++ src/app/ui/navbar/links/links.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-links',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './links.component.html',
   styleUrl: './links.component.css'
 })
