70823725c203c487848dbe4de362a08df447b249
Simon Flohr <sflohr2@wgu.edu>
2025-05-12 23:06:52 -0500

    feat: add resume button component
    - Adds a new component for displaying a resume button. This component will be used in the navbar.

---
+++ src/app/ui/navbar/resume-button/resume-button.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-resume-button',
+  imports: [],
+  templateUrl: './resume-button.component.html',
+  styleUrl: './resume-button.component.css'
+})
+export class ResumeButtonComponent {
+
+}
