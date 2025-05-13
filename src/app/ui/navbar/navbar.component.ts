ab7449652c2c2e2775dd1f1a8d71b10a7a3f5de4
Simon Flohr <sflohr2@wgu.edu>
2025-05-12 23:13:39 -0500

    feat: add navbar component to application layout
    - Adds a navbar component to the application layout.
    - Import NavbarComponent in AppComponent.
    - Import LinksComponent, LogoComponent, MobileToggleComponent, and ResumeButtonComponent in NavbarComponent.

--- src/app/ui/navbar/navbar.component.ts
+++ src/app/ui/navbar/navbar.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
+import { LinksComponent } from './links/links.component';
+import { LogoComponent } from './logo/logo.component';
+import { MobileToggleComponent } from './mobile-toggle/mobile-toggle.component';
+import { ResumeButtonComponent } from './resume-button/resume-button.component';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [
+    LinksComponent,
+    LogoComponent,
+    MobileToggleComponent,
+    ResumeButtonComponent
+  ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
