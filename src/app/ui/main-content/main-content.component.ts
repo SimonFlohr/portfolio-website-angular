5bc3ad8783d5fa1b6a695feee5ff3ae1764f08c0
Simon Flohr <sflohr2@wgu.edu>
2025-05-14 11:57:59 -0500

    feat: add hero section to main content
    - Introduces a hero section to the main content area.
    - Ensures the grid animation remains behind the hero content by adjusting its CSS properties and introducing a stacking context with z-index.

--- src/app/ui/main-content/main-content.component.ts
+++ src/app/ui/main-content/main-content.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { GridAnimationComponent } from './grid-animation/grid-animation.component';
+import { HeroComponent } from './hero/hero.component';
 
 @Component({
   selector: 'app-main-content',
-  imports: [GridAnimationComponent],
+  imports: [
+    GridAnimationComponent,
+    HeroComponent
+  ],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.css'
 })
