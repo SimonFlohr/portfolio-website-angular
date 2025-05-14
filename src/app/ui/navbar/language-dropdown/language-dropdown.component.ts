9d61182810fbb81192fbac2da4fe388bb6392338
Simon Flohr <sflohr2@wgu.edu>
2025-05-13 23:01:38 -0500

    feat: update language dropdown appearance
    - Improves the language dropdown's appearance by using a flag icon, a clearer label, and updated styling.
    - The component now uses `NgOptimizedImage` for better performance.

--- src/app/ui/navbar/language-dropdown/language-dropdown.component.ts
+++ src/app/ui/navbar/language-dropdown/language-dropdown.component.ts
@@ -1,8 +1,9 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-language-dropdown',
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './language-dropdown.component.html',
   styleUrl: './language-dropdown.component.css'
 })
