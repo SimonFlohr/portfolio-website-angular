0e74a6a834956c63bb2c310951d17316076df471
Simon Flohr <sflohr2@wgu.edu>
2025-05-16 20:18:46 -0500

    feat: enable image optimization for ProfilePicComponent
    - Uses the NgOptimizedImage directive to optimize image loading and improve performance.
    - Adjusts image dimensions to maintain aspect ratio.

--- src/app/ui/main-content/hero/profile-pic/profile-pic.component.ts
+++ src/app/ui/main-content/hero/profile-pic/profile-pic.component.ts
@@ -1,8 +1,11 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-profile-pic',
-  imports: [],
+  imports: [
+    NgOptimizedImage
+  ],
   templateUrl: './profile-pic.component.html',
   styleUrl: './profile-pic.component.css'
 })
