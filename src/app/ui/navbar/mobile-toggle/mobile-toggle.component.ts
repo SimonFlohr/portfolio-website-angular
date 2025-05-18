0e16a7bb95c8db6a3fa7cd2a2c938dc331d490b1
Simon Flohr <sflohr2@wgu.edu>
2025-05-17 23:43:57 -0500

    feat: add mobile toggle button
    - Implements a mobile toggle button with animations for the navbar.
        + The toggle button is displayed on smaller screens and allows users to open and close the mobile menu. The animation provides visual feedback when the button is toggled.

--- src/app/ui/navbar/mobile-toggle/mobile-toggle.component.ts
+++ src/app/ui/navbar/mobile-toggle/mobile-toggle.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
+import { animate } from 'animejs';
 
 @Component({
   selector: 'app-mobile-toggle',
@@ -7,5 +8,50 @@ import { Component } from '@angular/core';
   styleUrl: './mobile-toggle.component.css'
 })
 export class MobileToggleComponent {
+
+  isMenuOpen: boolean = false;
+
+  constructor(
+    private el: ElementRef<HTMLElement>
+  ) {}
+
+  toggleMobileMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+    if (this.isMenuOpen) {
+      this.animateMobileToggle(true);
+    } else {
+      this.animateMobileToggle(false);
+    }
+  }
+
+  animateMobileToggle(menuOpen: boolean): void {
+    const barOne = this.el.nativeElement.querySelector('#barOne') || this.el.nativeElement.firstElementChild as HTMLElement;
+    const barTwo = this.el.nativeElement.querySelector('#barTwo') || this.el.nativeElement.firstElementChild as HTMLElement;
+    const barThree = this.el.nativeElement.querySelector('#barThree') || this.el.nativeElement.firstElementChild as HTMLElement;
+
+    animate(barOne, {
+      translateY: menuOpen ? '14px' : '0px',
+      translateX: menuOpen ? '2px' : '0px',
+      rotate: menuOpen ? '-45deg' : '0deg',
+      width: menuOpen ? '44px' : '40px',
+      duration: 300,
+      ease: 'outElastic(1, .8)'
+    })
+
+    animate(barThree, {
+      translateY: menuOpen ? '-14px' : '0px',
+      translateX: menuOpen ? '2px' : '0px',
+      rotate: menuOpen ? '45deg' : '0deg',
+      width: menuOpen ? '44px' : '40px',
+      duration: 300,
+      ease: 'outElastic(1, 0.8)'
+    })
+
+    animate(barTwo, {
+      opacity: menuOpen ? 0 : 1,
+      duration: 100,
+      easing: 'outQuad'
+    })
+  }
 
 }
