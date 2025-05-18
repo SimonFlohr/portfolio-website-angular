3959a71c227076db7cf384a6fb0d2581a3a7a5ab
Simon Flohr <sflohr2@wgu.edu>
2025-05-18 17:59:56 -0500

    feat: implement mobile navbar animation
    - Implements the animation of the mobile navbar when it's opened or closed.
        + It uses `animate` function to smoothly transition the navbar in and out of view.

--- src/app/ui/navbar/navbar.component.ts
+++ src/app/ui/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 import { LinksComponent } from './links/links.component';
 import { LogoComponent } from './logo/logo.component';
 import { MobileToggleComponent } from './mobile-toggle/mobile-toggle.component';
@@ -20,9 +20,32 @@ export class NavbarComponent {
 
   isMenuOpen: boolean = false;
 
+  constructor(
+    private el: ElementRef<HTMLElement>
+  ) {}
+
   handleMenuData(menuOpen: boolean): void {
     this.isMenuOpen = menuOpen;
     console.log(this.isMenuOpen);
+    this.animateOnMenuStateChange(menuOpen);
+  }
+
+  animateOnMenuStateChange(menuOpen: boolean): void {
+    const mobileNavbar = this.el.nativeElement.querySelector('#mobile-navbar') || this.el.nativeElement.firstElementChild as HTMLElement;
+    
+    if (menuOpen) {
+      animate(mobileNavbar, {
+        translateX: -1030,
+        duration: 200,
+        easing: 'outElastic(1, .8)'
+      })
+    } else {
+      animate(mobileNavbar, {
+        translateX: 1030,
+        duration: 800,
+        easing: 'outElastic(1, .8)'
+      })
+    }
   }
 
 }
