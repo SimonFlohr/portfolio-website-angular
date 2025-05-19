71c10500df6a4d4bb778072909c77c62f5a55753
Simon Flohr <sflohr2@wgu.edu>
2025-05-19 02:44:17 -0500

    feat: add internationalization support to hero section
    - Adds content for Dutch language and displays content based on the selected language stored in local storage. Retrieves the language from local storage in ngOnInit.

--- src/app/ui/main-content/hero/hero.component.ts
+++ src/app/ui/main-content/hero/hero.component.ts
@@ -1,5 +1,4 @@
-import { Component, ElementRef, ViewChild, AfterViewInit, OnDestroy, HostListener } from '@angular/core';
-import { animate } from 'animejs';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { MagneticButtonDirective } from '../../../shared/directives/magnetic-button.directive';
 import { ProfilePicComponent } from "./profile-pic/profile-pic.component";
@@ -16,7 +15,13 @@ import { ProfilePicComponent } from "./profile-pic/profile-pic.component";
   styleUrl: './hero.component.css',
   standalone: true
 })
-export class HeroComponent {
+export class HeroComponent implements OnInit {
+
+  currLanguage: string | null = null;
+
+  ngOnInit(): void {
+    this.currLanguage = localStorage.getItem("language");
+  }
 
   // TODO -> Update resume link to actual resume
   resumeLink: string = "https://drive.google.com/file/d/1JyO4bwQOvopTW4-47_84WMZ-GqlAJLz7/view?usp=sharing";
