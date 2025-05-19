15b6130fb20e5e48e1dcbbf7ce934f2e38ea99be
Simon Flohr <sflohr2@wgu.edu>
2025-05-19 03:07:01 -0500

    feat: update navbar links to include Dutch subtitles
    - Updates the navbar links to include Dutch subtitles if the currently selected language is Dutch.
    
    This provides additional context for each link and improves the user experience on mobile devices.

--- src/app/ui/navbar/links/links.component.ts
+++ src/app/ui/navbar/links/links.component.ts
@@ -12,13 +12,14 @@ import { SubtleLinkAnimationDirective } from '../../../shared/directives/subtle-
   styleUrl: './links.component.css'
 })
 export class LinksComponent implements OnInit {
-  linkArray: string[] = [];
+  linkArray: string[][] = [];
 
   ngOnInit(): void {
     if (localStorage.getItem("language") === "nl-NL") {
-      this.linkArray = ["Introductie", "Opleidingen", "Projecten", "Contact"];
+      // this.linkArray = ["Introductie", "Opleidingen", "Projecten", "Contact"];
+      this.linkArray = [["Introductie", "// 127.0.0.1 is waar het hart ligt"], ["Opleidingen", "// opleidingen en cursussen die ik heb gevolgd"], ["Projecten", "// projecten die ik graag met de wereld deel"], ["Contact", "// mijn contactgegevens"]];
     } else {
-      this.linkArray = ["About", "Education", "Projects", "Contact"];
+      this.linkArray = [["About", "// there's no place like 127.0.0.1"], ["Education", "// how I got where I am now"], ["Projects", "// things I like to show off"], ["Contact", "// call me, maybe?"]];
     }
   }
 }
