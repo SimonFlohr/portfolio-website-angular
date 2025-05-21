0b5bf952d477eea339f13387e572704be9372b0f
Simon Flohr <sflohr2@wgu.edu>
2025-05-21 01:42:30 -0500

    feat(implementation): replace profile ring with animated SVG
    - Replaces the static profile picture ring with an animated SVG.
    
    This change improves the visual appeal of the profile section by adding a subtle, continuous animation to the ring surrounding the profile picture.

--- src/app/ui/main-content/hero/profile-pic/ring-animation/ring-animation.component.ts
+++ src/app/ui/main-content/hero/profile-pic/ring-animation/ring-animation.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import { animate } from 'animejs';
 
 @Component({
   selector: 'app-ring-animation',
@@ -6,6 +7,40 @@ import { Component } from '@angular/core';
   templateUrl: './ring-animation.component.html',
   styleUrl: './ring-animation.component.css'
 })
-export class RingAnimationComponent {
+export class RingAnimationComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+    this.animateRing();
+  }
 
+  animateRing() {
+    this.setTransformOrigin(document.getElementById('circleOne')!);
+    this.setTransformOrigin(document.getElementById('circleTwo')!);
+    this.setTransformOrigin(document.getElementById('circleThree')!);
+
+
+    animate('#circleOne', {
+      rotate: '360deg',
+      ease: 'linear',
+      loop: true,
+      duration: 100000,
+    });
+
+    animate('#circleTwo', {
+      rotate: '360deg',
+      ease: 'linear',
+      loop: true,
+      duration: 60000,
+    });
+
+    animate('#circleThree', {
+      rotate: '360deg',
+      ease: 'linear',
+      loop: true,
+      duration: 60000,
+    });
+  }
+
+  setTransformOrigin(element: HTMLElement) {
+    element.style.transformOrigin = 'center center';
+  }
 }
