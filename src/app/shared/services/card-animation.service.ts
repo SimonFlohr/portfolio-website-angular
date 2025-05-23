ae74b5ee2fb3443fa60ad5c30c8660ff7e6cc705
Simon Flohr <sflohr2@wgu.edu>
2025-05-23 18:01:12 -0500

    feat: implement card animations for education section
    - Implements card animations (cards 2 and 3) for the education section to enhance user experience.
    - Refactors the card animation service to manage multiple card elements using a map and animates the individual cards based on their unique IDs.
    - Updates the education component to utilize the card animation service and passes card-specific details to the service.

--- src/app/shared/services/card-animation.service.ts
+++ src/app/shared/services/card-animation.service.ts
@@ -1,41 +1,56 @@
 import { Injectable } from '@angular/core';
 import { animate } from 'animejs';
 
+interface CardElements {
+  card: HTMLElement;
+  cardReflection: HTMLElement;
+  cardTitle: HTMLElement;
+  cardBgImg: HTMLElement;
+  cardOverlayImg?: HTMLElement | null;
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class CardAnimationService {
 
+  private cardsMap: Map<number, CardElements> = new Map();
+
   card: HTMLElement | null = null;
   cardReflection: HTMLElement | null = null;
-  cardOverlayImg: HTMLElement | null = null;
   cardTitle: HTMLElement | null = null;
   cardBgImg: HTMLElement | null = null;
+  cardOverlayImg: HTMLElement | null = null;
 
   constructor() { }
 
-  setVars(
+  setCardElements(
+    cardId: number,
     card: HTMLElement,
     cardReflection: HTMLElement,
     cardTitle: HTMLElement,
     cardBgImg: HTMLElement,
     cardOverlayImg: HTMLElement | null = null
   ): void {
-    this.card = card;
-    this.cardReflection = cardReflection;
-    this.cardOverlayImg = cardOverlayImg;
-    this.cardTitle = cardTitle;
-    this.cardBgImg = cardBgImg;
+    this.cardsMap.set(cardId, {
+      card,
+      cardReflection,
+      cardTitle,
+      cardBgImg,
+      cardOverlayImg
+    });
   }
 
-  animateCard($event: MouseEvent, isEntered: boolean): void {
-      animate(this.card!, {
+  animateCard(cardId: number, isEntered: boolean): void {
+    const elements: any = this.cardsMap.get(cardId);
+
+      animate(elements.card, {
         scale: !isEntered ? 1.01 : 1,
         duration: 100,
         easing: 'easeOutElastic(.8, .5)'
       });
-      if (this.cardOverlayImg) {
-        animate(this.cardOverlayImg!, {
+      if (elements.cardOverlayImg) {
+        animate(elements.cardOverlayImg!, {
             translateX: !isEntered ? -10 : 0,
           scale: !isEntered ? 1.03 : 1,
           rotate: !isEntered ? -3 : 0,
@@ -43,13 +58,13 @@ export class CardAnimationService {
           easing: 'easeOutElastic(.8, .5)'
         });
       }
-      animate(this.cardReflection!, {
+      animate(elements.cardReflection!, {
         translateX: !isEntered ? -100 : 0,
         scale: !isEntered ? 1.3 : 1,
         duration: 150,
         easing: 'easeOutElastic(.8, .5)'
       });
-      animate(this.cardBgImg!, {
+      animate(elements.cardBgImg!, {
         scale: !isEntered ? 1.05 : 1,
         duration: 150,
         easing: 'easeOutElastic(.8, .5)'
--- src/app/ui/main-content/education/education.component.ts
+++ src/app/ui/main-content/education/education.component.ts
@@ -14,28 +14,65 @@ export class EducationComponent implements OnInit, AfterViewInit {
 
   currLanguage: string | null = null;
 
-  constructor(private cas: CardAnimationService) { }
+  constructor(
+    private cas: CardAnimationService
+  ) { }
 
   ngOnInit(): void {
     this.currLanguage = localStorage.getItem("language");
   }
 
   ngAfterViewInit(): void {
-      this.cas.setVars(
+      this.cas.setCardElements(
+        1,
         document.getElementById('education-card-1')!,
         document.getElementById('education-card-1-reflection-overlay-img')!,
         document.getElementById('education-card-1-text-container')!,
         document.getElementById('education-card-1-bg-img')!,
         document.getElementById('education-card-1-school-building-img')!
       );
+      this.cas.setCardElements(
+        2,
+        document.getElementById('education-card-2')!,
+        document.getElementById('education-card-2-reflection-overlay-img')!,
+        document.getElementById('education-card-2-text-container')!,
+        document.getElementById('education-card-2-bg-img')!
+      );
+      this.cas.setCardElements(
+        3,
+        document.getElementById('education-card-3')!,
+        document.getElementById('education-card-3-reflection-overlay-img')!,
+        document.getElementById('education-card-3-text-container')!,
+        document.getElementById('education-card-3-bg-img')!
+      );
   }
 
-  onMouseOver($event: MouseEvent): void {
-    this.cas.animateCard($event, false);
+  onMouseOver(cardNumber: number): void {
+    switch (cardNumber) {
+      case 1:
+        this.cas.animateCard(1, false);
+        break;
+      case 2:
+        this.cas.animateCard(2, false);
+        break;
+      case 3:
+        this.cas.animateCard(3, false);
+        break;
+    }
   }
 
-  onMouseLeave($event: MouseEvent): void {
-    this.cas.animateCard($event, true);
+  onMouseLeave(cardNumber: number): void {
+    switch (cardNumber) {
+      case 1:
+        this.cas.animateCard(1, true);
+        break;
+      case 2:
+        this.cas.animateCard(2, true);
+        break;
+      case 3:
+        this.cas.animateCard(3, true);
+        break;
+    }
   }
 
 }
