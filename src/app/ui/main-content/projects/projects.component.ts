cd50ea1c88ce2f885c4bee68da0ef532fcdc0afb
Simon Flohr <sflohr2@wgu.edu>
2025-05-25 09:29:52 -0500

    feat: implement project display with data binding
    - Populates the projects section with data from a project map,
    allowing for dynamic project information.
    - Introduces a project expansion feature, displaying detailed project
    information in an overlay.

--- src/app/ui/main-content/projects/projects.component.ts
+++ src/app/ui/main-content/projects/projects.component.ts
@@ -1,6 +1,19 @@
 import { NgOptimizedImage } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { MagneticButtonDirective } from '../../../shared/directives/magnetic-button.directive';
+import { animate } from 'animejs';
+
+export interface Project {
+  title: string,
+  shortDescription: string,
+  longDescription: string,
+  imagePath: string,
+  technologies: string[],
+  links: {
+    github: string,
+    live: string,
+  }
+}
 
 @Component({
   selector: 'app-projects',
@@ -14,9 +27,55 @@ import { MagneticButtonDirective } from '../../../shared/directives/magnetic-but
 export class ProjectsComponent implements OnInit {
 
   currLanguage: string | null = null;
+  projectMap: Map<number, Project> = new Map();
+  isProjectExpanded: boolean = false;
+
+  // Project entry: Portfolio Website
+  project0: Project = {
+    title: "Portfolio Website",
+    shortDescription:
+      "The website you are currently on. This project serves to showcase my UI/UX and frontend development skills. The emphasis is interactivity, minimalism, and responsiveness.",
+    longDescription:
+      "The website you are currently on. This project serves to showcase my UI/UX and frontend development skills. The emphasis is interactivity, minimalism, and responsiveness.",
+    imagePath: "img/project-0-bg.jpg",
+    technologies: ["Angular", "Tailwind CSS", "anime.js", "Git", "Figma", "TypeScript", "HTML", "CSS"],
+    links: {
+      github: "https://github.com/joshuawong99/portfolio",
+      live: "https://joshuawong99.github.io/portfolio/",
+    }
+  }
 
   ngOnInit(): void {
     this.currLanguage = localStorage.getItem("language");
+    this.addProject(0, this.project0);
+  }
+
+  addProject(id: number, project: Project) {
+    this.projectMap.set(id, project);
+  }
+
+  toggleProjectExpand() {
+    const projectExpandContainer = document.getElementById('project-expand-container');
+
+    if (!this.isProjectExpanded) {
+      this.isProjectExpanded = !this.isProjectExpanded;
+      projectExpandContainer?.classList.toggle('hidden');
+      animate(projectExpandContainer!, {
+        opacity: 1,
+        duration: 150,
+        easing: 'easeOutExpo'
+      })
+    } else if (this.isProjectExpanded) {
+      this.isProjectExpanded = !this.isProjectExpanded;
+      animate(projectExpandContainer!, {
+        opacity: 0,
+        duration: 150,
+        easing: 'easeOutExpo'
+      })
+      setTimeout(() => {
+        projectExpandContainer?.classList.toggle('hidden');
+      }, 150);
+    }
   }
 
 }
